581dc09d6aec8b09d8ca204b6621faa2bbc130d9
sabosantech <sabosan.tech@gmail.com>
2021-11-23 21:38:56 +0900

    first

---
+++ main.py
@@ -0,0 +1,31 @@
+import pyautogui as pg
+import pyperclip
+
+position = pg.position()
+print(position)
+
+
+pg.moveTo(x=456,y=87,duration=3) # form pos
+# pg.typewrite(‘youtube’, 0.1)
+
+pg.click(x=456,y=87,duration=1)
+
+pg.typewrite('a.png')
+
+# pyautogui.typewrite(‘a.png’)
+
+pg.click(x=171,y=87,duration=1)
+
+
+pg.click(x=171,y=87,duration=1)
+
+pg.click(x=171,y=87,duration=1)
+
+pg.click(x=171,y=87,duration=1)
+
+pg.click(x=171,y=87,duration=1)
+
+# pg.typewrite(‘excel’, 0.1)
+
+
+# Point(x=1253, y=455)
